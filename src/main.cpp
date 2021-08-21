895a8436013c92b74c4aad1843fe8bc9a8836af0
0xvpr <malikrb@live.com>
2021-08-20 19:42:58 -0500

    file structure changed

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <windows.h>
+#include <stdlib.h>
+#include <stdio.h.>
+
+DWORD WINAPI MainThread(LPVOID lpReserved)
+{
+	FILE* fp;
+	AllocConsole();
+	freopen_s(&fp, "CONOUT$", "w", stdout);
+
+	printf("Success! or whatever\n");
+	while (!GetAsyncKeyState(VK_END)) 
+	{
+		//Do nothing
+	}
+
+	return TRUE;
+}
+
+BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
+{
+	switch (dwReason)
+	{
+		case DLL_PROCESS_ATTACH:
+			DisableThreadLibraryCalls((HMODULE)hInstance);
+			CreateThread(0, 0, MainThread, 0, 0, 0);
+			break;
+		default:
+			break;
+	}
+
+	return TRUE;
+}
